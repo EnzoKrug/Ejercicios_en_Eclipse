026f3e8a937c56f4c86695a8b50103ed8cfc82cf
EnzoKrug <enzo.nkrug@hotmail.com>
2020-04-27 13:53:36 -0300

    Ejercicio 1

---
+++ Ejercicio1_Arrays/main.c
@@ -0,0 +1,112 @@
+/*
+ ============================================================================
+ Name        : Ejercicio.c
+ Author      :
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+/*
+*   EJERCICIO DE ARRAYS Y FUNCIONES
+*	de un array de n elementos enteros debemos obtener:
+*
+*	1- necesito saber si existe un número en el array
+*	(ej: {2,66,6,7,2,4,7,2,2,4} ,
+*	a-el 6 existe
+*	b-el 9 no existe
+*	2- necesito saber cuantas veces aparece un número en el array
+*	(ej: {2,66,6,7,2,4,7,2,2,4} ,el 4 esta dos veces
+*	3- necesito buscar un número y que me retorne el indice del mismo
+*	(ej:{2,66,6,7,2,4,7,2,2,4} , el indice de 66 es 1)
+*	4- el número máximo ingresado
+*	5- el número mínimo ingresado
+*	6- necesito saber la cantidad veces que esta ese máximo en el array
+*	(ej: {2,66,6,7,2,4,7,2,2,4} en este array el máximo esta dos veces)
+*	7- necesito saber la cantidad veces que esta ese mínimo en el array
+*	(ej: {2,66,6,7,2,4,7,2,2,4}en este array el minimo esta cuatro veces)
+*
+*	Definir las funciones necesarias con los parametros que usted crea necesario
+*	cargar el array con los siguientes datos = {2,66,6,7,2,4,7,2,2,4}
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define T 10
+
+int ValidarNumeroExistenteInt(int arrayParametro[], int tam, int auxiliar);
+
+void MostrarArray(char mensaje[], int arrayParametro[], int tam);
+
+int main()
+{
+    system("color F0"); //Manera para cambiar de color la consola.
+	int arrayNumeros[10]={2,66,6,7,2,4,7,2,2,4};
+
+	int valorDeRetorno;
+	int auxiliarInt=8;
+
+	MostrarArray("El numero #", arrayNumeros, T);
+
+	//printf("\n\nLos numeros ya están cargados, ingrese alguno, para ver si existe o no: \n\n");
+	//scanf("%d",&auxiliarInt);
+	valorDeRetorno = ValidarNumeroExistenteInt(arrayNumeros, T, auxiliarInt );
+	if(valorDeRetorno == 0)
+	{
+		printf("El %d existe!!", auxiliarInt);
+	}
+	else if(valorDeRetorno == 1)
+		{
+			printf("No se ah encontrado el %d en los numeros cargados.", auxiliarInt);
+		}
+	else
+	{
+		printf("fffff");
+	}
+    system("pause");
+	return EXIT_SUCCESS;
+}
+
+void MostrarArray(char mensaje[], int arrayParametro[], int tam)
+{
+	//int retorno = -1;
+	int i;
+
+	if(arrayParametro != NULL && tam >= 0)
+	{
+		//retorno = 0;
+		for(i=0;i<tam;i++)
+		{
+			printf("%s%d: %3d\n",mensaje, i, arrayParametro[i]);
+		}
+	}
+	//return retorno;
+}
+
+
+int ValidarNumeroExistenteInt(int arrayParametro[], int tam, int auxiliar)
+{
+	int i;
+	int retorno = -1;
+
+	if(arrayParametro != NULL && tam >= 0)
+	{
+		for(i=0;i<tam;i++)
+		{
+			if(arrayParametro[i] == auxiliar)
+			{
+				retorno = 1;
+			}
+			else
+			{
+				if(arrayParametro[i] != auxiliar)
+				{
+					retorno = 0;
+				}
+			}
+		}
+	}
+	return retorno;
+}
